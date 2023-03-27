d21c814c5261446b91606bba794fc3199aaa9da9
Mauricio Guerrero <65383117+Mg4145@users.noreply.github.com>
2023-03-26 23:26:12 -0400

    Formatted both README and Python file

--- README.md
+++ README.md
@@ -1,24 +1,42 @@
 # website-update-checker
 
-Simple script that take an url and make an md5 an sha256 out of it. Check if it changed since last time.
+Simple script that take an url and make an md5 an sha256 out of it.
+Check if it changed since last time.
 
-## ideas of implementation of this program
-- check if an awesome github page changed since last time you consulted it.
-- check if a download page as changed, that could imply a change of version.
+## Ideas
+- Check if an awesome github page changed since last time you consulted it.
+- Check if a download page as changed, that could imply a change of version.
 - etc...
 
 
-## How to use
+## Usage
 
-- with command line arguments ```python website-checker.py arg1 arg2 arg3```, ```python website-checker.py -h ``` for help
-- with a ```site to analyze``` file ```python website-checker.py --file path_to_the_file``` (supports tags). For configuration look at the template.ini file at the repository root.
+* For configuration look at the template.ini file at the repository root.
 
-## Little problems
+Running with arguments:
 
-some website change everytime (google for exemple) => take only the body?
+```python
+website-checker.py arg1 arg2 arg3
+```
 
-## to implement
+Help running:
 
+```python
+website-checker.py -h
+```
+
+With website to analyze (supports tags):
+
+```python
+website-checker.py --file path_to_the_file
+```
+
+## Problems
+
+- Some website change everytime (Google for example) => take only the body?
+
+## Implement
 
 - make a specific outfile for the user?
-- implement a last_change_only option (w/ argparse) that record only if the last md5 /sha256 is different
+- implement a last_change_only option (w/ argparse) that record only if the
+  last md5 /sha256 is different
--- website-checker.py
+++ website-checker.py
@@ -13,12 +13,13 @@ from src.funct import change_since_last_time, file_path
 
 
 REGEX_FOR_URL = re.compile(
-    r'^(?:http|ftp)s?://' # http:// or https://
-    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
-    r'localhost|' #localhost...
-    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
-    r'(?::\d+)?' # optional port
-    r'(?:/?|[/?]\S+)$', re.IGNORECASE
+    r"^(?:http|ftp)s?://"  # http:// or https://
+    r"(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|"  # domain...
+    r"localhost|"  # localhost...
+    r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"  # ...or ip
+    r"(?::\d+)?"  # optional port
+    r"(?:/?|[/?]\S+)$",
+    re.IGNORECASE,
 )
 
 START_PROGRAM_TIME = time.time()
@@ -28,9 +29,27 @@ DB_FILEPATH = os.path.join(os.path.dirname(__file__), "output", "changes_db.json
 if __name__ == "__main__":
     # Argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("-u","--url_list", nargs='+',default=[], help="List of url to analyse. Wrong urls will be ignored.")
-    parser.add_argument("-o","--open_url", default=False, action="store_true", help="Boolean, if true it opens the url in your preferred webbrowser")
-    parser.add_argument("-f","--file", nargs=1, type=file_path, help="Specifies a path to a file with a list of browsers in it.")
+    parser.add_argument(
+        "-u",
+        "--url_list",
+        nargs="+",
+        default=[],
+        help="List of url to analyse. Wrong urls will be ignored.",
+    )
+    parser.add_argument(
+        "-o",
+        "--open_url",
+        default=False,
+        action="store_true",
+        help="Boolean, if true it opens the url in your preferred webbrowser",
+    )
+    parser.add_argument(
+        "-f",
+        "--file",
+        nargs=1,
+        type=file_path,
+        help="Specifies a path to a file with a list of browsers in it.",
+    )
     args = parser.parse_args()
     urls_from_file = []
     if args.file is not None:
@@ -38,7 +57,7 @@ if __name__ == "__main__":
         config.read(args.file[0])
         # we catch the different tags
         tags = config.sections()
-        #for now we just extract every url
+        # for now we just extract every url
 
         for a_tag in config.sections():
             for a_nickname in config[a_tag]:
@@ -46,8 +65,19 @@ if __name__ == "__main__":
     urls_combined = set(urls_from_file + args.url_list)
     print(urls_combined)
     # validate urls
-    valid_urls = [an_url for an_url in urls_combined if all([getattr(urlparse(an_url), qualifying_attr) for qualifying_attr in ('scheme', 'netloc')])]
-    valid_urls = [an_url[:-1] if an_url.endswith("/") else an_url for an_url in valid_urls]
+    valid_urls = [
+        an_url
+        for an_url in urls_combined
+        if all(
+            [
+                getattr(urlparse(an_url), qualifying_attr)
+                for qualifying_attr in ("scheme", "netloc")
+            ]
+        )
+    ]
+    valid_urls = [
+        an_url[:-1] if an_url.endswith("/") else an_url for an_url in valid_urls
+    ]
     print(f"Kept valid urls : {valid_urls}")
     # reading the changes DB in order to take them in account if we already scanned the url.
     with open(DB_FILEPATH, "r") as readable:
@@ -66,14 +96,19 @@ if __name__ == "__main__":
         if url not in changes:
             changes[url] = {}
 
-        #changes[url].append(current_time)
-        changes[url][current_time] = {"md5": md5_res.hexdigest(), "sha256": sha256_res.hexdigest()}
+        # changes[url].append(current_time)
+        changes[url][current_time] = {
+            "md5": md5_res.hexdigest(),
+            "sha256": sha256_res.hexdigest(),
+        }
 
     # writing the data back in the file
     with open(DB_FILEPATH, "w+") as open_file:
         changes = json.dump(changes, open_file, indent=4)
 
-    changed_urls = change_since_last_time(urls=valid_urls, db_file_path=DB_FILEPATH, start_time=START_PROGRAM_TIME)
+    changed_urls = change_since_last_time(
+        urls=valid_urls, db_file_path=DB_FILEPATH, start_time=START_PROGRAM_TIME
+    )
 
     print(f"new : {changed_urls}")
     if args.open_url:
